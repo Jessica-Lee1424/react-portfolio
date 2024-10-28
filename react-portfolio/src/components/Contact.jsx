9d4527666bd235643a2595be0eb5dbc5d3098168
Jessica Gonnella <jessicagonnella@Jessicas-MacBook-Air.local>
2024-10-28 07:44:24 -0400

    Committs initial code

---
+++ react-portfolio/src/components/Contact.jsx
@@ -0,0 +1,20 @@
+const Content = () => {
+    return (
+        <main>
+            <section id="about">
+                <h2>About Me</h2>
+                <p>This is where you can write about yourself.</p>
+            </section>
+            <section id="projects">
+                <h2>Projects</h2>
+                <p>This is where you can showcase your projects.</p>
+            </section>
+            <section id="contact">
+                <h2>Contact</h2>
+                <p>This is where you can provide your contact information.</p>
+            </section>
+        </main>
+    );
+};
+
+export default Content;
