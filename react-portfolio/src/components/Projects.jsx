e201aaeb62710c41a9e8bdb37b29f764cff8d621
Jessica Gonnella <jessicagonnella@Jessicas-MacBook-Air.local>
2024-10-30 15:24:13 -0400

    Commits CSS updates

--- react-portfolio/src/components/Projects.jsx
+++ react-portfolio/src/components/Projects.jsx
@@ -18,41 +18,42 @@ function Projects() {
                   Here is an example of a challenge I successfully completed. This final code for this project can be reviewed here: 
                   <a href="https://github.com/Jessica-Lee1424/SQL-Employee-Tracker" target="_blank" rel="noopener noreferrer">https://github.com/Jessica-Lee1424/SQL-Employee-Tracker</a>
               </p>
+              <img src="/employeeTracker.png" alt="Employee Tracker" className="project-image" />
           </div>
 
           <div className="project-container">
               <h4>Weather Service API Using Databases and APIs</h4>
-              <img src="/weatherAPI.png" alt="Weather API" className="project-image" />
               <p>
                   This is an example of a challenge that lived up to its name in that I was very challenged and have a lot of room for improvement here. The project in this repository needs to be reworked. My goal is to return to this code before the course program ends and correct the errors to get the API working as expected. <br />
                   <a href="https://github.com/Jessica-Lee1424/Weather-Service_API" target="_blank" rel="noopener noreferrer">https://github.com/Jessica-Lee1424/Weather-Service_API</a>
               </p>
+              <img src="/weatherAPI.png" alt="Weather API" className="project-image" />
           </div>
 
           <div className="project-container">
               <h4>Build A Car</h4>
-              <img src="/carDemo.png" alt="Car Demo" className="project-image" />
               <p>
                   Here we updated an existing TypeScript command-line application that builds and uses cars to include additional options for motorbikes and trucks.
                   <a href="https://github.com/Jessica-Lee1424/Build-a-Car" target="_blank" rel="noopener noreferrer">https://github.com/Jessica-Lee1424/Build-a-Car</a>
               </p>
+              <img src="/carDemo.png" alt="Car Demo" className="project-image" />
           </div>
 
           <div className="project-container">
-              <h4>My First Blog</h4>
-              <img src="/readMe.png" alt="My First Blog" className="project-image" />
+              <h4>My First Blog</h4>              
               <p>
                   This challenge required us to modify starter code to complete a personal blog. We were required to include a content form that dynamically rendered blog posts. Also required was a light/dark mode toggle.
                   <a href="https://github.com/Jessica-Lee1424/My-First-Blog" target="_blank" rel="noopener noreferrer">https://github.com/Jessica-Lee1424/My-First-Blog</a>
               </p>
+              <img src="/readMe.png" alt="My First Blog" className="project-image" />
           </div>
 
           <div className="project-container">
               <h4>README Generator</h4>
-              <img src="/readMe.png" alt="README Generator" className="project-image" />
               <p>
                   In this project, we modified starter code to create a command-line application that dynamically generates a professional README.md file based on user input using the Inquirer package.
                   <a href="https://github.com/Jessica-Lee1424/README_Generator" target="_blank" rel="noopener noreferrer">https://github.com/Jessica-Lee1424/README_Generator</a>
+                  <img src="/readMe.png" alt="README Generator" className="project-image" />
               </p>
           </div>
       </section>
